1f914bde10e3278b4803a3d8d52e2dc1eed44867
43B69 <cem4ik05@yandex.ru>
2024-04-20 00:39:04 +0300

    FIRST UPDATE (added api)

--- data/books_api.py
+++ data/books_api.py
@@ -3,42 +3,83 @@ from flask import jsonify, make_response, request
 from . import db_session
 from .books import BOOK
 
-BOOKS_BLUEPRINT_GET_USING_ID = flask.Blueprint("BOOK_api_ID", __name__)
-BOOKS_BLUEPRINT_GET_ALL = flask.Blueprint("BOOK_api_ALL", __name__)
+API_GET_BOOK_ON_ID = flask.Blueprint("API_ID", __name__)
+API_GET_ALL_BOOKS = flask.Blueprint("API_ALL", __name__)
 BOOKS_BLUEPRINT_FILTER = flask.Blueprint("BOOK_api_filter", __name__)
+FILTER_NAMES = ["find", "name", "description", "author", "publisher", "genres", "codecs", "country"]
 
 
-@BOOKS_BLUEPRINT_GET_USING_ID.route("/api/books/<int:book_id>", methods=["GET"])
+def formatter(inp_data):
+    res = {}
+    for i in inp_data:
+        res[int(i.ID)] = [i.BOOK_NAME, i.BOOK_ALTER_NAME, i.AUTHOR, i.PUBLISHER, i.DESCRIPTION, i.OUT_DATE,
+                          i.OUT_COUNTRY, [int(j) for j in i.GENRES.split("_")], i.FILE_NAME,
+                          [int(j) for j in i.CODECS.split("_")], i.CODECS_HASH, i.DOWNLOADS]
+    return res
+
+
+@API_GET_BOOK_ON_ID.route("/api/books/<int:book_id>", methods=["GET"])
 def BOOK_api_ID(book_id):
     db_sess = db_session.create_session()
     query_book = db_sess.query(BOOK).filter(BOOK.ID == book_id).first()
     if not query_book:
         return make_response(jsonify({"error": "not found"}), 404)
-    return jsonify({query_book.ID: [query_book.BOOK_NAME, query_book.BOOK_ALTER_NAME, query_book.AUTHOR,
+    return jsonify({int(query_book.ID): [query_book.BOOK_NAME, query_book.BOOK_ALTER_NAME, query_book.AUTHOR,
                                     query_book.PUBLISHER, query_book.DESCRIPTION, query_book.OUT_DATE,
                                     query_book.OUT_COUNTRY, query_book.GENRES, query_book.FILE_NAME,
                                     query_book.CODECS, query_book.CODECS_HASH, query_book.DOWNLOADS,
                                     query_book.CREATE_DATE]})
 
 
-@BOOKS_BLUEPRINT_GET_ALL.route("/api/books/all", methods=["GET"])
+@API_GET_ALL_BOOKS.route("/api/books/all", methods=["GET"])
 def BOOK_api_ALL():
     db_sess = db_session.create_session()
-
     query_all_books = db_sess.query(BOOK).all()
     if not query_all_books:
         return make_response(jsonify({"error": "not found"}), 404)
     res = {}
     for i in query_all_books:
-        res[i.ID] = [i.BOOK_NAME, i.BOOK_ALTER_NAME, i.AUTHOR, i.PUBLISHER, i.DESCRIPTION, i.OUT_DATE,
+        res[int(i.ID)] = [i.BOOK_NAME, i.BOOK_ALTER_NAME, i.AUTHOR, i.PUBLISHER, i.DESCRIPTION, i.OUT_DATE,
                      i.OUT_COUNTRY, i.GENRES, i.FILE_NAME, i.CODECS, i.CODECS_HASH, i.DOWNLOADS, i.CREATE_DATE]
     return jsonify(res)
 
 
 @BOOKS_BLUEPRINT_FILTER.route("/api/filter", methods=["GET"])
 def BOOK_api_filter():
-    args = request.args
-
+    args = request.args.to_dict()
+    # Буфер для обработки массивов
+    buff = 0
+    # Выписываем только правильные аргументы
+    right_args = {i: args[i] for i in args if i in FILTER_NAMES}
+    # Начинаем работать с сессией
     db_sess = db_session.create_session()
-    query_all_books = db_sess.query(BOOK).filter()
-    return args.to_dict()
+    query_all_books = db_sess.query(BOOK)
+    if "find" in right_args and right_args["find"] != "":
+        if "name" in right_args and right_args["name"] == "1":
+            query_all_books = query_all_books.filter(BOOK.BOOK_NAME.like(f"%{right_args["find"]}%"))
+        if "description" in right_args and right_args["description"] == "1":
+            query_all_books = query_all_books.filter(BOOK.DESCRIPTION.like(f"%{right_args["find"]}%"))
+        if "author" in right_args and right_args["author"] == "1":
+            query_all_books = query_all_books.filter(BOOK.AUTHOR.like(f"%{right_args["find"]}%"))
+        if "publisher" in right_args and right_args["publisher"] == "1":
+            query_all_books = query_all_books.filter(BOOK.PUBLISHER.like(f"%{right_args["find"]}%"))
+    data = formatter(query_all_books.all())
+    if "genres" in right_args and right_args["genres"] != "":
+        # Жанры, если что идут через точку с запятой
+        buff = right_args["genres"].split("_")
+        genres = []
+        for i in range(len(buff)):
+            try:
+                int(buff[i])
+            except ValueError:
+                return make_response(jsonify({"error": "The genre number is a string"}), 400)
+            genres.append(int(buff[i]))
+        for i in genres:
+            if i in
+            query_all_books = query_all_books.filter(BOOK.GENRES.like(f"{i}"))
+
+    res = {}
+    for i in data:
+        res[int(i.ID)] = [i.BOOK_NAME, i.BOOK_ALTER_NAME, i.AUTHOR, i.PUBLISHER, i.DESCRIPTION, i.OUT_DATE,
+                          i.OUT_COUNTRY, i.GENRES, i.FILE_NAME, i.CODECS, i.CODECS_HASH, i.DOWNLOADS, i.CREATE_DATE]
+    return jsonify(res)
--- main.py
+++ main.py
@@ -9,9 +9,18 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 def main():
     db_session.global_init("db/main.db")
-    app.register_blueprint(BOOKS_BLUEPRINT_GET_USING_ID)
-    app.register_blueprint(BOOKS_BLUEPRINT_GET_ALL)
+    app.register_blueprint(API_GET_BOOK_ON_ID)
+    app.register_blueprint(API_GET_ALL_BOOKS)
     app.register_blueprint(BOOKS_BLUEPRINT_FILTER)
+    '''b = BOOK()
+    b.BOOK_NAME = "HUIGMA"
+    b.GENRES = "_2_6_7_"
+    b.AUTHOR = "BEBRA"
+    b.PUBLISHER = "OPIDA"
+    b.DESCRIPTION = "PIGHA"
+    db_sess = db_session.create_session()
+    db_sess.add(b)
+    db_sess.commit()'''
     app.run()
 
 
